f2894d7738a1ee4d80cdde49a7d10d1d708f4414
Edwin Bonilla <edwin.bonilla@nicta.com.au>
2014-09-19 18:32:17 +1000

    evaluating up-to-date Trungs code

--- code/inference/variational/opper/evaluateOpperDan.m
+++ code/inference/variational/opper/evaluateOpperDan.m
@@ -2,10 +2,10 @@ function  Perf = evaluateOpperDan(  )
 %EVALUATEOPPERDAN  Evaluate predictions on Dan's benchmarks
 %   Detailed explanation goes here
 DATADIR    = 'dataDan';
-RESULTSDIR =  'resultsOpper';
+RESULTSDIR =  'resultsTrung';
 
 %benchmarks = {'lineardata', 'poly3data', 'expdata', 'sindata', 'tanhdata'};
-benchmarks = {'lineardata', 'poly3data', 'expdata'};
+benchmarks = {'lineardata'};
 
 
 L = length(benchmarks);
